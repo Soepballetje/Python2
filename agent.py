08bec2ca9448a6e047a2e54c8146e9cbde9ac6b4
Warner24 <Wardbakker@gmail.com>
2015-12-16 11:49:29 +0100

    agent en management

--- agent.py
+++ agent.py
@@ -35,18 +35,18 @@ def get_value(number):
     # Example in which a PowerShell script is used. The STDOUT is used to pass results back to python.
     # Exporting with export-csv and reading the CSV using Python is also possible of course.
     if number == 4:
-        p=subprocess.Popen(['powershell.exe',    # Atlijd gelijk of volledig pad naar powershell.exe
+        p = subprocess.Popen(['powershell.exe',  # Atlijd gelijk of volledig pad naar powershell.exe
             '-ExecutionPolicy', 'Unrestricted',  # Override current Execution Policy
-            'c:\\scripts\\agent_counters.ps1'],  # Naam van en pad naar je PowerShell script
-        stdout=subprocess.PIPE)                  # Zorg ervoor dat je de STDOUT kan opvragen.
+            'C:\\Python\\agent_counters.ps1'],   # Naam van en pad naar je PowerShell script
+            stdout=subprocess.PIPE)              # Zorg ervoor dat je de STDOUT kan opvragen.
         output = p.stdout.read()                 # De stdout
         return output
 
     # Example of sing a PowerShell oneliner. Useful for simple PowerShell commands.
     if number == 5:
-        p=subprocess.Popen(['powershell',
+        p = subprocess.Popen(['powershell',
             "get-service | measure-object | select -expandproperty count"],
-        stdout=subprocess.PIPE)                  # Zorg ervoor dat je de STDOUT kan opvragen.
+            stdout=subprocess.PIPE)              # Zorg ervoor dat je de STDOUT kan opvragen.
         output = p.stdout.read()                 # De stdout
         return output
 
@@ -57,7 +57,7 @@ def get_value(number):
 # ---------------------------------------------------------
 
 # do not change anything unless you know what you're doing.
-port=8008
+port = 8008
 dispatcher = SoapDispatcher(
     'my_dispatcher',
     location = "http://localhost:8008/",
@@ -68,8 +68,8 @@ dispatcher = SoapDispatcher(
 
 # do not change anything unless you know what you're doing.
 dispatcher.register_function('get_value', get_value,
-    returns={'resultaat': str},   # return data type
-    args={'number': int}         # it seems that an argument is mandatory, although not needed as input for this function: therefore a dummy argument is supplied but not used.
+    returns = {'resultaat': str},   # return data type
+    args = {'number': int}         # it seems that an argument is mandatory, although not needed as input for this function: therefore a dummy argument is supplied but not used.
     )
 
 # Let this agent listen forever, do not change anything unless needed.
