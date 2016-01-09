254ae4b49160d9e6b8a0f8b1b03190dfc003d25b
Ward Bakker <Wardbakker@gmail.com>
2016-01-09 16:24:00 +0100

    ok

--- grafiek.py
+++ grafiek.py
@@ -1,10 +1,11 @@
 import csv
 import matplotlib.pyplot as plt
-import datetime
+
 
 list_Ram1 = []
 list_Ram2 = []
 list_Ram3 = []
+gebruik = []
 Time = []
 a = [len(Time)]
 
@@ -13,7 +14,8 @@ for row in input_file:
     Time.append(str(row["Time"]))
     list_Ram1.append(float(row["RAM %"]))
     list_Ram2.append(float(row["RAM Geheugen Vrij"]))
-    list_Ram3.append((float(row["RAM totaal"] - float(row["RAM Geheugen Vrij"]))))
+    list_Ram3.append(float(row["RAM totaal"]))
+    gebruik.append(list_Ram3[-1] - list_Ram2[-1])
 
 print list_Ram1
 print list_Ram2
@@ -22,17 +24,16 @@ print list_Ram3
 plt.figure(1)
 plt.subplot(211)
 plt.plot(list_Ram2, label='Vrije Ram geheugen')
-plt.plot(list_Ram3, label='Totale Ram geheugen')
+plt.plot(gebruik, label='Totale Ram geheugen')
+plt.title('Ram statistics')
 plt.legend()
 
 plt.subplot(212)
 plt.plot(list_Ram1, label='Ram percentage')
-
 plt.xlabel('Tijd')
-plt.ylabel('Hoeveelheid')
-plt.title('Ram statistics')
-plt.xticks(a, Time, rotation='vertical')
+plt.ylabel('Percentage %')
 
 plt.legend()
-# plt.savefig('ram.png')
+
+plt.savefig('ram.png')
 plt.show()
