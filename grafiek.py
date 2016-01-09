cb1082efdef1e64a56e37280f9c2613286d554e3
Ward Bakker <Wardbakker@gmail.com>
2016-01-09 14:03:08 +0100

    ok

--- grafiek.py
+++ grafiek.py
@@ -1,12 +1,14 @@
 import csv
 import matplotlib.pyplot as plt
+import datetime.strptime
 
 list_Ram1 = []
 list_Ram2 = []
 list_Ram3 = []
-input_file = csv.DictReader(open("Roadrunner.csv"))
+Time = []
+input_file = csv.DictReader(open("Desktop-Ward.csv"))
 for row in input_file:
-    Time = float(row["Time"])
+    Time.append(str(row["Time"]))
     list_Ram1.append(float(row["RAM %"]))
     list_Ram2.append(float(row["RAM Geheugen Vrij"]))
     list_Ram3.append(float(row["RAM totaal"]))
@@ -14,13 +16,17 @@ for row in input_file:
 print list_Ram1
 print list_Ram2
 print list_Ram3
+print Time
 
-plt.plot(list_Ram1, label='Ram percentage')
+plt.plot(list_Ram1, label='Ram percentage', a)
 plt.plot(list_Ram2, label='Vrije Ram geheugen')
 plt.plot(list_Ram3, label='Totale Ram geheugen')
-plt.xlabel(str(Time))
+plt.xlabel('Tijd')
 plt.ylabel('Hoeveelheid')
 plt.title('Ram statistics')
+dates = plt.dates.date2num(Time)
+plot_date(dates, values)
+
 plt.legend()
 plt.show()
 plt.savefig()
