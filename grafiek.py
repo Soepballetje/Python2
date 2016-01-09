c453c9e1b82f19a0758c1e45361c44d25e94eb14
Ward Bakker <Wardbakker@gmail.com>
2016-01-09 16:31:13 +0100

    ok

--- grafiek.py
+++ grafiek.py
@@ -1,25 +1,17 @@
 import csv
 import matplotlib.pyplot as plt
 
-
 list_Ram1 = []
 list_Ram2 = []
 list_Ram3 = []
 gebruik = []
-Time = []
-a = [len(Time)]
 
 input_file = csv.DictReader(open("Desktop-Ward.csv"))
 for row in input_file:
-    Time.append(str(row["Time"]))
     list_Ram1.append(float(row["RAM %"]))
     list_Ram2.append(float(row["RAM Geheugen Vrij"]))
     list_Ram3.append(float(row["RAM totaal"]))
     gebruik.append(list_Ram3[-1] - list_Ram2[-1])
-
-print list_Ram1
-print list_Ram2
-print list_Ram3
 
 plt.figure(1)
 plt.subplot(211)
@@ -32,7 +24,6 @@ plt.subplot(212)
 plt.plot(list_Ram1, label='Ram percentage')
 plt.xlabel('Tijd')
 plt.ylabel('Percentage %')
-
 plt.legend()
 
 plt.savefig('ram.png')
